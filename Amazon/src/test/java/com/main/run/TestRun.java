203d3beacd290e0f818ec7e78c22840b1d6762ef
Sumon <Sumon@home.fios-router.home>
2019-02-07 19:57:23 -0500

    Amazon in git

---
+++ Amazon/src/test/java/com/main/run/TestRun.java
@@ -0,0 +1,17 @@
+package com.main.run;
+
+import com.generic.library.GenericClass;
+
+public class TestRun extends GenericClass {
+
+	public static void main(String[] args) {
+		TestRun obj = new TestRun();
+		
+		obj.getSetup();
+		obj.getLogin();
+		
+	
+
+	}
+
+}
